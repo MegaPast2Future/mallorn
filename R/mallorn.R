a1594ef1813d31e27bb1a5c488e615c8a8e2e15e
Matt Davis <matt.davis@bios.au.dk>
2017-08-17 12:52:27 +0200

    Updated mallorn description to include new function name expected.ed

--- R/mallorn.R
+++ R/mallorn.R
@@ -1,8 +1,8 @@
 #' Compute Expected Evolutionary Distinctiveness
 #'
-#' This is simple package to calculate Expected Evolutionary Distinctiveness (EED) for each tip of a phylogenetic tree.
+#' This is simple package to calculate Expected Evolutionary Distinctiveness (expected ED) for each tip of a phylogenetic tree.
 #'
-#' Currently, \pkg{mallorn} has only one function \code{\link{eed}}, which is used to calculate Expected Evolutionary Distinctiveness, the expected amount of unique evolutionary history belonging to each taxa.
+#' Currently, \pkg{mallorn} has only one function \code{\link{expected.ed}}, which is used to calculate Expected Evolutionary Distinctiveness, the expected amount of unique evolutionary history belonging to each taxa.
 #' @import ape
 #' @import matrixStats
 #' @import picante
