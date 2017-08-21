d38d8224293a9561d60476303f36960e9bc3f5d2
Matt Davis <matt.davis@bios.au.dk>
2017-08-21 10:39:34 +0200

    Update package description to include ePD

--- R/mallorn.R
+++ R/mallorn.R
@@ -1,8 +1,9 @@
 #' Compute Expected Evolutionary Distinctiveness
 #'
-#' This is simple package to calculate Expected Evolutionary Distinctiveness (expected ED) for each tip of a phylogenetic tree.
+#' This is simple package to calculate expected Phylogenetic Diversity (expected PD) and expected Evolutionary Distinctiveness (expected ED) for each tip of a phylogenetic tree.
 #'
-#' Currently, \pkg{mallorn} has only one function \code{\link{eED}}, which is used to calculate expected Evolutionary Distinctiveness (expected ED), the expected amount of unique evolutionary history belonging to each taxa.
+#' Currently, \pkg{mallorn} has two functions: \code{\link{ePD}} and \code{\link{eED}}. \code{\link{ePD}} calculates expected PD, the expected amount of evolutionary history represented by a community. \code{\link{eED}} calculates expected ED, the expected amount of unique evolutionary history belonging to each taxon.
+#'
 #' @import ape
 #' @import matrixStats
 #' @import picante
