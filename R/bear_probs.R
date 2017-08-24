bba1232417f6732533ec55c9f2484b8f366e15a4
Matt Davis <matt.davis@bios.au.dk>
2017-08-24 10:59:15 +0200

    Changed bear extinction probabilities to presence probabilities
    This is because the input of the functions will be changed to probabilites that species are present

--- R/bear_probs.R
+++ R/bear_probs.R
@@ -1,6 +1,6 @@
-#' Extinction probabilites for bears.
+#' Probabilites bears are present.
 #'
-#' Example extinction probabilities for late Quaternary bear species.
+#' Example probabilities of presence (1 - extinction probabilities) for late Quaternary bear species.
 #'
 #'
 #' @format A named vector with 13 bear species. The names match the tip labels for \code{\link{bear_tree}}, an included dated phylogeny.
