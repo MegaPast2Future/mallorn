32c42ffdb3f984ab4b5a2b3464688d682209d7e6
Matt Davis <matt.davis@bios.au.dk>
2017-08-24 11:55:02 +0200

    Change bear extinction probabilities to presence probabilities

--- R/bear_matrix.R
+++ R/bear_matrix.R
@@ -1,6 +1,6 @@
-#' Extinction probabilites for several communities of bears.
+#' Probabilities bears are present in several communities.
 #'
-#' Example extinction (i.e. non occurence) probabilities for late Quaternary bear species for the whole world, Eurasia, and North America.
+#' Example presence probabilities (1 - extinction probabilities) for late Quaternary bear species for the whole world, Eurasia, and North America.
 #'
 #'
 #' @format A taxa by community matrix with 13 bear species for rows. The names match the tip labels for \code{\link{bear_tree}}, an included, dated phylogeny. The columns represent 3 different modern communities of bears: Global, Eurasia, and North America.
--- R/bear_probs.R
+++ R/bear_probs.R
@@ -1,6 +1,6 @@
 #' Probabilites bears are present.
 #'
-#' Example probabilities of presence (1 - extinction probabilities) for late Quaternary bear species.
+#' Example presence probabilities (1 - extinction probabilities) for late Quaternary bear species.
 #'
 #'
 #' @format A named vector with 13 bear species. The names match the tip labels for \code{\link{bear_tree}}, an included dated phylogeny.
